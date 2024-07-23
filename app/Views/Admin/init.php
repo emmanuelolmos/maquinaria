27dc10db56f1638120841e5556fd908cca2326a9
Emmanuel <emmanuel.raol@outlook.com>
2024-07-23 11:04:14 -0600

    Adición de archivo sweetaler.js

--- app/Views/Admin/init.php
+++ app/Views/Admin/init.php
@@ -22,7 +22,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
     <link rel="stylesheet" href="../../../assets/css/bootstrap.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="../../../assets/js/sweetalert/sweetalert2@11.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
 </head>
 <body>
